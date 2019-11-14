82eb40e7c6ee6e11f13f9faeaa67a16843f1c66a
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-11-13 21:34:18 -0500

    stuff

---
+++ client/build/precache-manifest.1ab334d903b0cf095535cb75db7c32de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e7cc0dbe429572de8ca22d0542843f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79378e444e9e209e1dfe",
+    "url": "/static/css/main.e6c8f720.chunk.css"
+  },
+  {
+    "revision": "19ccb91f71319c74c7e3",
+    "url": "/static/js/2.533307eb.chunk.js"
+  },
+  {
+    "revision": "79378e444e9e209e1dfe",
+    "url": "/static/js/main.0d5581e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4207132a3a8dbca1db478abbe05c6791",
+    "url": "/static/media/barbershop.4207132a.png"
+  }
+]);
