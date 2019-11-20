baa6844059907e4f72c3bd3d7fe7a20a83caf884
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-11-20 10:49:16 -0500

    put picture and view profile button on appended online barbers

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://jarrod:Theblobfest1@ds041248.mlab.com:41248/heroku_vn5c35sj",
+    mongoURI: "mongodb://localhost:27017/shortcutzdb",
     cypher: "blahblah"
 };
